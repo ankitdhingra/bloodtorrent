458095c00a0ecacd5ddf9a7cedc03fc81612b3a7
Ankit Dhingra <ankitsdhingra@gmail.com>
2013-01-29 17:26:24 +0530

    <Ankit> Putting in the basic template generated using calatrava

---
+++ droid/bloodtorrent/src/com/bloodtorrent/Bloodtorrent.java
@@ -0,0 +1,23 @@
+package com.bloodtorrent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.calatrava.bridge.Launcher;
+
+public class Bloodtorrent extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Launcher.launchKernel("com.bloodtorrent", this, getApplication(), new Runnable() {
+          public void run() {
+            Launcher.launchFlow("example.converter.start");
+          }
+        });
+    }
+}
