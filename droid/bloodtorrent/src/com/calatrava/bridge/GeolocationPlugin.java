a18f786b4f98dd80108d1b97d8595add3fae7d83
Akshay Mankar <akshaym@thoughtworks.com>
2013-04-13 22:25:40 +0530

    [#17] - Getting 'Best Location' from GPS or Network

--- droid/bloodtorrent/src/com/calatrava/bridge/GeolocationPlugin.java
+++ droid/bloodtorrent/src/com/calatrava/bridge/GeolocationPlugin.java
@@ -18,6 +18,7 @@ import java.util.Map;
 @CalatravaPlugin(name = "geolocation")
 public class GeolocationPlugin implements RegisteredPlugin
 {
+    public static final String TAG = GeolocationPlugin.class.getSimpleName();
     private PluginRegistry registry;
     private Context ctxt;
     private String successCallback;
@@ -27,40 +28,20 @@ public class GeolocationPlugin implements RegisteredPlugin
     {
         this.registry = registry;
         this.ctxt = ctxt;
-        Log.d(GeolocationPlugin.class.getSimpleName(), "<<<<<<<<<<<<<<<<<< Installiing Geolocation >>>>>>>>>>>>>>>>>>>>>");
+        Log.d(TAG, "<<<<<<<<<<<<<<<<<< Installiing Geolocation >>>>>>>>>>>>>>>>>>>>>");
         registry.installCommand("geolocation", new PluginCommand() {
             @Override
             public void execute(Intent action, RegisteredActivity frontmost) {
-                // Acquire a reference to the system Location Manager
-                LocationManager locationManager = (LocationManager) GeolocationPlugin.this.ctxt.getSystemService(Context.LOCATION_SERVICE);
+                Location location = getBestLocation();
+                Log.d(TAG, "<<<<<<<<<<<<<<<<<< Location Changed !!! >>>>>>>>>>>>>>>>>");
+                Log.d(TAG, "<<<<<<<<<<<<<<The Accuracy is: " + location.getAccuracy() + ">>>>>>>>>>>>>>>>");
+                Map<String, Object> geolocation = new HashMap<String, Object>();
+                HashMap<String, String> coords = new HashMap<String, String>();
 
-                // Define a listener that responds to location updates
-                LocationListener locationListener = new LocationListener() {
-                    public void onLocationChanged(Location location) {
-                        // Called when a new location is found by the network location provider.
-                        Log.d(GeolocationPlugin.class.getSimpleName(), "<<<<<<<<<<<<<<<<<< Location Changed !!! >>>>>>>>>>>>>>>>>");
-                        Map<String, Object> geolocation = new HashMap<String, Object>();
-                        HashMap<String, String> coords = new HashMap<String, String>();
-
-                        coords.put("latitude", String.valueOf(location.getLatitude()));
-                        coords.put("longitude", String.valueOf(location.getLongitude()));
-                        geolocation.put("coords", coords);
-                        GeolocationPlugin.this.registry.invokeCallback(successCallback, geolocation);
-                    }
-
-                    public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-                    public void onProviderEnabled(String provider) {}
-
-                    public void onProviderDisabled(String provider) {}
-                };
-
-                // Register the listener with the Location Manager to receive location updates
-
-//                for (String provider : locationManager.getProviders(true)) {
-                    Log.d(GeolocationPlugin.class.getSimpleName(), "<<<<<<<<<<<<<<<<<< Requesting Location to GPS Provider >>>>>>>>>>>>>>>>>");
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-//                }
+                coords.put("latitude", String.valueOf(location.getLatitude()));
+                coords.put("longitude", String.valueOf(location.getLongitude()));
+                geolocation.put("coords", coords);
+                GeolocationPlugin.this.registry.invokeCallback(successCallback, geolocation);
             }
         });
     }
@@ -71,5 +52,70 @@ public class GeolocationPlugin implements RegisteredPlugin
         failureCallback = (String)args.get("failureCallbackHandle");
         Log.d(GeolocationPlugin.class.getSimpleName(), "<<<<<<<<<<<<<<<<<< Calling Geolocation >>>>>>>>>>>>>>>>>");
         ctxt.sendBroadcast(registry.pluginCommand("geolocation"));
+    }
+
+
+    private Location getBestLocation() {
+        Location gpslocation = getLocationByProvider(LocationManager.GPS_PROVIDER);
+        Location networkLocation = getLocationByProvider(LocationManager.NETWORK_PROVIDER);
+
+
+        //if we have only one location available, the choice is easy
+        if (gpslocation == null) {
+            Log.d(TAG, "No GPS Location available.");
+            return networkLocation;
+        }
+        if (networkLocation == null) {
+            Log.d(TAG, "No Network Location available");
+            return gpslocation;
+        }
+
+        //a locationupdate is considered 'old' if its older than the configured update interval. this means, we didn't get a
+        //update from this provider since the last check
+        long old = System.currentTimeMillis() - 1800000; //That's 30 minutes
+        boolean gpsIsOld = (gpslocation.getTime() < old);
+        boolean networkIsOld = (networkLocation.getTime() < old);
+
+        //gps is current and available, gps is better than network
+        if (!gpsIsOld) {
+            Log.d(TAG, "Returning current GPS Location");
+            return gpslocation;
+        }
+
+        //gps is old, we can't trust it. use network location
+        if (!networkIsOld) {
+            Log.d(TAG, "GPS is old, Network is current, returning network");
+            return networkLocation;
+        }
+
+        // both are old return the newer of those two
+        if (gpslocation.getTime() > networkLocation.getTime()) {
+            Log.d(TAG, "Both are old, returning gps(newer)");
+            return gpslocation;
+        } else {
+            Log.d(TAG, "Both are old, returning network(newer)");
+            return networkLocation;
+        }
+    }
+
+    /**
+     * get the last known location from a specific provider (network/gps)
+     */
+    private Location getLocationByProvider(String provider) {
+        Location location = null;
+
+        LocationManager locationManager = (LocationManager) ctxt
+                .getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            if (locationManager.isProviderEnabled(provider)) {
+
+                location = locationManager.getLastKnownLocation(provider);
+
+            }
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, "Cannot access Provider " + provider);
+        }
+        return location;
     }
 }
